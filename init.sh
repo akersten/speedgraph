69f826f44d7d00d60a16ccb646efa5d9dbc58ba4
Alex Kersten <alex@kersten.email>
2015-05-06 23:31:20 -0500

    add first bit of framework

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Grab a python implementation of speedtest
+wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest_cli.py
+chmod +x speedtest-cli
+
+
